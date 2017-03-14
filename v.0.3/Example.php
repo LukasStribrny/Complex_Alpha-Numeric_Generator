2a26507312425a544932e5bc3d1ba1eb3c7272af
Lukas Stribrny <LukasStribrny@users.noreply.github.com>
2017-03-14 23:19:12 +0000

    Update Example.php

--- v.0.3/Example.php
+++ v.0.3/Example.php
@@ -5,10 +5,10 @@ $UniqueKey->SetLength(8);
 $UniqueKey->SetType(5);
 
 $UniqueKey->CodeInput();
-$return = $UniqueKey->Generate();
+$return = $UniqueKey->Generate_String();
 print_r($return);
 
 $UniqueKey->CodeInput($return['code_base']);
-$return = $UniqueKey->Generate();
+$return = $UniqueKey->Generate_String();
 print_r($return);
 ?>
