5a9c79dbfa9b56c72d940e7a22144bfd5d918d3f
Lukas Stribrny <LukasStribrny@users.noreply.github.com>
2017-12-08 14:12:32 +0000

    Update Example_Microsoft_SN_Generate_Random_JSON.php

--- v.0.3/Base_Examples_NoMySQL/Example_Microsoft_SN_Generate_Random_JSON.php
+++ v.0.3/Base_Examples_NoMySQL/Example_Microsoft_SN_Generate_Random_JSON.php
@@ -2,5 +2,6 @@
 include('../Base_CANG_Generators/MS_G.php');
 $MS_G = new MS_G;
 $return = $MS_G->byRAND();//In 30 seconds you should be able to generate the key by this ID
-print_r($return);
+header("Content-type:application/json");
+echo json_encode($return,true)
 ?>
