d87b3aa6b55818c5f4cb61ed699eb4ce39eb88d1
Lukas Stribrny <LukasStribrny@users.noreply.github.com>
2017-12-08 13:58:27 +0000

    Update and rename v.0.3/Example_Generate_ID.php to v.0.3/Base_Examples_NoMySQL/Example_Generate_ID.php

--- v.0.3/Example_Generate_ID.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('CANG.php');
-$CANG = new CANG;
-$CANG->SetLength(8);
-$CANG->SetType(5);
-$return = $CANG->Generate_ID(249996);//You should be able to generate the key by any input ID
-print_r($return);
-?>
